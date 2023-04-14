9aeac27c90ba491148bd4baf1d163051f4bc7939
NbtKmy <nobuta77to99@hotmail.com>
2023-04-14 09:38:54 +0200

    some changes

--- src/loader.js
+++ src/loader.js
@@ -1,18 +1,3 @@
-/*
-document.onreadystatechange = function() {
-    if (document.readyState !== "complete") {
-        document.querySelector(
-          "main").style.visibility = "hidden";
-        document.querySelector(
-          "#lds-ellipsis").style.visibility = "visible";
-    } else {
-        document.querySelector(
-          "#lds-ellipsis").style.display = "none";
-        document.querySelector(
-          "main").style.visibility = "visible";
-    }
-};
-*/
 
 function end_loader() {
     var loader = document.querySelector(".lds-ellipsis");
