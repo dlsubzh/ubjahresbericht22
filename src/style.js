50661a0cf02a27344f0581e33ed1dcd19301b44b
NbtKmy <nobuta77to99@hotmail.com>
2023-04-19 23:43:20 +0200

    button position changed

--- src/style.js
+++ src/style.js
@@ -1,3 +1,7 @@
+///////////////////////
+/// canvas position ///
+///////////////////////
+
 const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 //console.log(width);
 let canvas = document.getElementById("canvas");
@@ -10,25 +14,19 @@ canvas.style.height = String(width * 1.3) + "px";
 canvas.style.left = String(adjust) + "px";
 canvas.style.top = "0px";
 
-//////////////////
-//  Popup       //
-//////////////////
+/////////////
+//  Popup  //
+/////////////
 
 const video = document.getElementById("video");
 const closeBtn = document.getElementById("closeBtn");
-const popup = document.getElementById("popupContent");
 
-popupTop = popup.offsetTop;
-videoTop = video.offsetTop;
-videoLeft = video.offsetLeft;
-videoHeight = video.offsetHeight;
-videoWidth = video.offsetWidth;
-closeBtnTop = (videoTop - popupTop) *0.5 - 200;
-//closeBtnTop = videoTop*0.5 - 150; 
-closeBtnLeft = videoLeft*0.5 - 100;
+const videoRect = video.getBoundingClientRect(); 
+videoTop = videoRect.top;
+videoLeft = videoRect.left;
 
-console.log(popupTop);
-console.log(videoTop);
+closeBtnTop = videoTop - 33;
+closeBtnLeft = videoLeft + 30;
 
 closeBtn.style.top = String(closeBtnTop) + "px";
 closeBtn.style.left = String(closeBtnLeft) + "px";
